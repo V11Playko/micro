23efa0c35271eac7fe7aab17306d4a85d2eead46
Heinner Vega <heinnervega20@gmail.com>
2024-04-18 17:30:10 -0500

    Add conexion a base de datos

---
+++ src/test/java/com/micro/demo/MicroApplicationTests.java
@@ -0,0 +1,13 @@
+package com.micro.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MicroApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
