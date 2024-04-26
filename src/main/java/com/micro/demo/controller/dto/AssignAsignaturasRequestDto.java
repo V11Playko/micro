c2e1e0bd57bd0ca7690ae6d50d163df2c078a31f
Heinner Vega <heinnervega20@gmail.com>
2024-04-26 17:36:00 -0500

    Add assign and remove asignatura

---
+++ src/main/java/com/micro/demo/controller/dto/AssignAsignaturasRequestDto.java
@@ -0,0 +1,20 @@
+package com.micro.demo.controller.dto;
+
+import java.util.List;
+
+import jakarta.validation.constraints.NotNull;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class AssignAsignaturasRequestDto {
+    @NotNull
+    private Long pensumId;
+    @NotNull
+    private List<Long> asignaturasId;
+}
---
+++ src/main/java/com/micro/demo/controller/dto/RemoveAsignaturaRequestDto.java
@@ -0,0 +1,18 @@
+package com.micro.demo.controller.dto;
+
+import jakarta.validation.constraints.NotNull;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class RemoveAsignaturaRequestDto {
+    @NotNull
+    private Long pensumId;
+    @NotNull
+    private Long asignaturaId;
+}
