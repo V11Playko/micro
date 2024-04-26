344aa58a76e10b733790bf9276a2824fae6e25b6
Heinner Vega <heinnervega20@gmail.com>
2024-04-26 17:36:39 -0500

    Add assign and remove docente dto

---
+++ src/main/java/com/micro/demo/controller/dto/RemoveDocenteRequestDto.java
@@ -0,0 +1,21 @@
+package com.micro.demo.controller.dto;
+
+
+import jakarta.validation.constraints.NotNull;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class RemoveDocenteRequestDto {
+
+    @NotNull(message = "El ID de la asignatura no puede ser nulo")
+    private Long asignaturaId;
+
+    @NotNull(message = "El correo del docente no puede ser nulo")
+    private String correoDocente;
+}
