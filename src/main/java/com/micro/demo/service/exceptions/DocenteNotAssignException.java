94a7f39a3e8c9d835d37af95cfd69c196d9fa51e
Heinner Vega <heinnervega20@gmail.com>
2024-04-26 17:34:13 -0500

    Add new exceptions

---
+++ src/main/java/com/micro/demo/service/exceptions/DocenteNotAssignException.java
@@ -0,0 +1,7 @@
+package com.micro.demo.service.exceptions;
+
+public class DocenteNotAssignException extends RuntimeException{
+    public DocenteNotAssignException() {
+        super();
+    }
+}
---
+++ src/main/java/com/micro/demo/service/exceptions/DocenteNotFound.java
@@ -0,0 +1,7 @@
+package com.micro.demo.service.exceptions;
+
+public class DocenteNotFound extends RuntimeException{
+    public DocenteNotFound() {
+        super();
+    }
+}
