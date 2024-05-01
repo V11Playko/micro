689b8844d430aa246d9a9261cb2ae51d570345cb
Heinner Vega <heinnervega20@gmail.com>
2024-05-01 14:49:57 -0500

    Add forma basica cuadriculada con alguna informacion incluida

--- src/main/java/com/micro/demo/service/impl/PdfService.java
+++ src/main/java/com/micro/demo/service/impl/PdfService.java
@@ -18,6 +18,7 @@ import org.apache.pdfbox.pdmodel.PDPage;
 import org.apache.pdfbox.pdmodel.PDPageContentStream;
 import org.springframework.stereotype.Service;
 
+import java.awt.*;
 import java.io.File;
 import java.io.IOException;
 import java.time.LocalDate;
@@ -52,108 +53,150 @@ public class PdfService implements IPdfService {
 
         String fileName = generateFileName(pensum);
 
-        // Define la ubicación de la carpeta de descargas
-        String downloadFolder = System.getProperty("user.home") + File.separator + "Downloads";
-
-        // Verifica si el archivo ya existe y ajusta el nombre si es necesario
-        File file = new File(downloadFolder, fileName);
-        int count = 1;
-        while (file.exists()) {
-            fileName = generateFileNameWithIndex(pensum, count++);
-            file = new File(downloadFolder, fileName);
-        }
-
-
         try (PDDocument document = new PDDocument()) {
-            PDType1Font titleFont = PDType1Font.HELVETICA_BOLD;
-            int titleFontSize = 35;
-            PDType1Font font = PDType1Font.HELVETICA;
-            int fontSize = 12;
-
-            PDPage page = new PDPage(PDRectangle.A4);
+            PDPage page = new PDPage();
             document.addPage(page);
 
             try (PDPageContentStream contentStream = new PDPageContentStream(document, page)) {
-                contentStream.setFont(titleFont, titleFontSize);
-                contentStream.beginText();
-
-                float pageWidth = page.getMediaBox().getWidth();
-                float textWidth = titleFont.getStringWidth(programaAcademico.getNombre()) / 1000 * titleFontSize;
-                float titleX = (pageWidth - textWidth) / 2; // Centrar el título
-                float yPosition = PDRectangle.A4.getHeight() - 50; // Empieza en la parte superior de la página
-                contentStream.newLineAtOffset(titleX, yPosition);
-                contentStream.showText(programaAcademico.getNombre());
-                yPosition -= 30; // Avanza a la siguiente línea con más espacio
-
-                contentStream.setFont(font, fontSize); // Cambiar al font regular
-
-                contentStream.newLineAtOffset(0, -60);
-                contentStream.showText("Director: " + programaAcademico.getDirector().getCorreo());
-                yPosition -= 20;
-
-                contentStream.newLineAtOffset(0, -20);
-                contentStream.showText("Pensum ID: " + pensum.getId());
-                yPosition -= 20;
-
-                contentStream.newLineAtOffset(0, -20);
-                contentStream.showText("Fecha de inicio: " + pensum.getFechaInicio().format(DateTimeFormatter.ofPattern("yyyy-MM-dd")));
-                yPosition -= 20;
-
-                contentStream.newLineAtOffset(0, -20);
-                contentStream.showText("Fecha final: " + pensum.getFechaFinal().format(DateTimeFormatter.ofPattern("yyyy-MM-dd")));
-                yPosition -= 20;
-
-                contentStream.newLineAtOffset(0, -20);
-                contentStream.showText("Créditos totales: " + pensum.getCreditosTotales());
-                yPosition -= 20;
-
-                contentStream.newLineAtOffset(0, -20);
-                contentStream.showText("Asignaturas:");
-                yPosition -= 20;
-
                 // Obtener los nombres de todas las asignaturas asociadas al pensum
                 List<AsignaturaPensum> asignaturaPensums = asignaturaPensumRepository.findByPensumId(pensumId);
-                for (AsignaturaPensum asignaturaPensum : asignaturaPensums) {
-                    String asignaturaNombre = asignaturaPensum.getAsignatura().getNombre();
-                    Integer codigoAsignatura = asignaturaPensum.getAsignatura().getCodigo();
-                    String tipoCursoAsignatura = asignaturaPensum.getAsignatura().getTipoCurso();
-                    String metodologiaAsignatura = asignaturaPensum.getAsignatura().getMetodologia();
-                    String asignaturaPredecesora = asignaturaPensum.getAsignatura().getAsignaturaPredecesora();
-                    String asignaturaSucesora = asignaturaPensum.getAsignatura().getAsignaturaSucesora();
 
-                    contentStream.newLineAtOffset(0, -30);
-                    contentStream.showText("- " + asignaturaNombre);
-                    yPosition -= 30;
 
-                    contentStream.newLineAtOffset(0, -20); // Ajuste para los detalles de la asignatura
-                    contentStream.showText("    Código: " + codigoAsignatura);
-                    yPosition -= 20;
+                // Definir el tamaño de la tabla y las celdas
+                float margin = 50;
+                float yStart = page.getMediaBox().getHeight() - margin;
+                float tableWidth = page.getMediaBox().getWidth() - 2 * margin;
+                float cellHeight = 20f;
+                float tableMargin = 5f;
 
-                    contentStream.newLineAtOffset(0, -20);
-                    contentStream.showText("    Tipo de Curso: " + tipoCursoAsignatura);
-                    yPosition -= 20;
+                // Definir el número de columnas y su ancho
+                float[] columnWidths = {50, 80, 30, 30, 30, 30, 60, 30, 30, 30, 80};
+                float tableHeight = cellHeight * 2 + tableMargin;
+                float yPosition = yStart;
 
-                    contentStream.newLineAtOffset(0, -20);
-                    contentStream.showText("    Metodología: " + metodologiaAsignatura);
-                    yPosition -= 20;
+                // Dibujar las líneas horizontales y verticales de la tabla
+                contentStream.setLineWidth(0.5f);
+                float xEnd = margin + tableWidth;
+                float yEnd = yStart - tableHeight;
+                float nextX = margin;
 
-                    contentStream.newLineAtOffset(0, -20);
-                    contentStream.showText("    Asignatura Predecesora: " + asignaturaPredecesora);
-                    yPosition -= 20;
-
-                    contentStream.newLineAtOffset(0, -20);
-                    contentStream.showText("    Asignatura Sucesora: " + asignaturaSucesora);
-                    yPosition -= 20;
+                // Dibujar las líneas verticales
+                for (int i = 0; i < columnWidths.length; i++) {
+                    contentStream.moveTo(nextX, yStart);
+                    contentStream.lineTo(nextX, yEnd - cellHeight * (asignaturaPensums.size() -1)); // Ajustado para que termine antes de la última línea horizontal
+                    contentStream.stroke();
+                    nextX += columnWidths[i];
                 }
 
-                contentStream.endText();
+                // Dibujar una línea horizontal encima de los títulos
+                contentStream.moveTo(margin, yStart);
+                contentStream.lineTo(xEnd, yStart);
+                contentStream.stroke();
+
+                // Ajustar yEnd para que llegue hasta la última línea horizontal
+                yEnd -= (asignaturaPensums.size() + 1) * cellHeight;
+
+                // Dibujar las líneas horizontales
+                for (int i = 0; i < asignaturaPensums.size() + 1; i++) {
+                    float y = yStart - (tableHeight / 2) - (i * cellHeight);
+                    contentStream.moveTo(margin, y);
+                    contentStream.lineTo(xEnd, y);
+                    contentStream.stroke();
+                }
+
+                // Dibujar la última línea vertical proporcional al número de filas
+                float lastVerticalLineYStart = yStart - (tableHeight / 2) - ((asignaturaPensums.size()) * cellHeight);
+                contentStream.moveTo(xEnd, yStart);
+                contentStream.lineTo(xEnd, lastVerticalLineYStart);
+                contentStream.stroke();
+
+
+
+                // Escribir los encabezados de las columnas
+                float nextXHeader = margin;
+                contentStream.setFont(PDType1Font.HELVETICA_BOLD, 8); // Fuente Helvetica Bold para los encabezados
+                contentStream.setNonStrokingColor(Color.BLACK);
+                contentStream.setLeading(15f);
+                yStart -= 15; // Ajustar la posición hacia abajo
+                for (int i = 0; i < columnWidths.length; i++) {
+                    float columnWidth = columnWidths[i];
+                    float textX = nextXHeader + 5; // Alineación a la izquierda con un pequeño margen
+                    if (i == 0) {
+                        textX -= -2; // Ajuste adicional hacia la izquierda para el primer encabezado
+                    } else if (i == 1) {
+                        textX -= -18;
+                    } else if (i == 6) {
+                        textX -= -12;
+                    } else {
+                        textX = nextXHeader + (columnWidth / 2) - 5; // Alinear en el centro de la columna
+                    }
+                    contentStream.beginText();
+                    contentStream.setFont(PDType1Font.HELVETICA_BOLD, 8); // Cambiar la fuente a Helvetica Bold
+                    contentStream.newLineAtOffset(textX, yStart);
+                    contentStream.showText(getHeaderTitle(i));
+                    contentStream.endText();
+                    nextXHeader += columnWidth;
+                }
+
+                // Escribir los datos de las asignaturas en la tabla
+                for (AsignaturaPensum asignaturaPensum : asignaturaPensums) {
+                    Integer codigoAsignatura = asignaturaPensum.getAsignatura().getCodigo();
+                    String asignaturaNombre = asignaturaPensum.getAsignatura().getNombre();
+                    String htiAsignatura = asignaturaPensum.getAsignatura().getHti();
+
+                    // Escribir la fila de datos
+                    float nextXData = margin;
+                    float lineHeight = 18f; // Altura de línea para separar las filas
+                    String[] rowData = {codigoAsignatura.toString(), asignaturaNombre, htiAsignatura};
+                    for (int i = 0; i < rowData.length; i++) { // Iterar sobre el tamaño correcto de rowData
+                        float columnWidth = columnWidths[i];
+                        float textX = margin + 5; // Alineación a la izquierda
+                        if (i == 0) {
+                            textX = margin + (columnWidth / 2) - 20; // Alineación centrada para la primera columna
+                        } else {
+                            textX = nextXData + 5; // Alineación a la izquierda para las demás columnas
+                        }
+                        String rowDataText = rowData[i];
+                        contentStream.beginText();
+                        contentStream.setFont(PDType1Font.HELVETICA, 8); // Cambiar la fuente a Helvetica normal
+                        contentStream.newLineAtOffset(textX, yStart - lineHeight);
+                        contentStream.showText(rowDataText != null ? rowDataText : ""); // Manejo de valores nulos
+                        contentStream.endText();
+                        nextXData += columnWidth;
+                    }
+                    yStart -= cellHeight;
+                }
             }
 
-            // Concatena el nombre del archivo al directorio de descargas
-            String filePath = downloadFolder + File.separator + fileName;
-
-            // Guarda el documento en la ubicación especificada
-            document.save(filePath);
+            document.save(fileName);
+        }
+    }
+    private String getHeaderTitle(int index) {
+        switch (index) {
+            case 0:
+                return "Código";
+            case 1:
+                return "Nombre";
+            case 2:
+                return "Ht";
+            case 3:
+                return "Hp";
+            case 4:
+                return "Hti";
+            case 5:
+                return "Cre";
+            case 6:
+                return "Prereq";
+            case 7:
+                return "Sim";
+            case 8:
+                return "Rc";
+            case 9:
+                return "Te";
+            case 10:
+                return "Equis";
+            default:
+                return "";
         }
     }
 
@@ -162,13 +205,6 @@ public class PdfService implements IPdfService {
         String pensumNumero = String.valueOf(pensum.getId());
         String fechaInicio = formatDate(pensum.getFechaInicio());
         return programaAcademicoNombre + "_Pensum" + pensumNumero + "_" + fechaInicio + ".pdf";
-    }
-
-    private String generateFileNameWithIndex(Pensum pensum, int index) {
-        String programaAcademicoNombre = pensum.getProgramaAcademico().getNombre();
-        String pensumNumero = String.valueOf(pensum.getId());
-        String fechaInicio = formatDate(pensum.getFechaInicio());
-        return programaAcademicoNombre + "_Pensum" + pensumNumero + "_" + fechaInicio + "_" + index + ".pdf";
     }
 
     private String formatDate(LocalDate date) {
