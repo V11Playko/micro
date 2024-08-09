709b1639e04cd14751c548495a75885c022f5181
Heinner Vega <heinnervega20@gmail.com>
2024-08-08 21:46:32 -0500

    Add AsignaturaMapper

---
+++ src/main/java/com/micro/demo/mapper/AsignaturaMapper.java
@@ -0,0 +1,14 @@
+package com.micro.demo.mapper;
+
+import com.micro.demo.controller.dto.AsignaturaDto;
+import com.micro.demo.entities.Asignatura;
+import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
+import org.mapstruct.factory.Mappers;
+@Mapper(componentModel = "spring")
+public interface AsignaturaMapper {
+    @Mapping(source = "areaFormacionId", target = "areaFormacion.id")
+    @Mapping(source = "competenciaId", target = "competencia.id")
+    @Mapping(target = "preRequisitos", ignore = true) // Manejo manual en el servicio
+    Asignatura toEntity(AsignaturaDto dto);
+}
