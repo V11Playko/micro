ee88ecd3175f2f03dad09bd854b2775a4043670c
Heinner Vega <heinnervega20@gmail.com>
2024-12-05 07:01:22 -0500

    Se modifico el frontend y authcontroller

--- auth-service/src/main/java/com/playko/auth/client/UserClient.java
+++ auth-service/src/main/java/com/playko/auth/client/UserClient.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
 @FeignClient(name = "micro-service",
-        url = "http://localhost:8091",
+        url = "http://ALB-micro-1831133009.us-east-1.elb.amazonaws.com",
         configuration = FeignClientInterceptor.class)
 public interface UserClient {
     @GetMapping("/public/getUserByEmail")
